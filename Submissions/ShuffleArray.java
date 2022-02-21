48c7de0d9dde4c7048b329584f6219ae8932ae6c
shxyv <ilgar.shixiyevv@gmail.com>
2022-02-21 14:55:27 +0400

    Added ShuffleArray solution

---
+++ Submissions/ShuffleArray.java
@@ -0,0 +1,20 @@
+package Submissions;
+
+import java.util.Arrays;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(shuffle(new int[]{2, 5, 1, 3, 4, 7}, 3)));
+    }
+
+    public static int[] shuffle(int[] nums, int n) {
+        int [] newArr = new int[nums.length];
+        int j=0;
+        for (int i=0;i<n;i++){
+
+            newArr[j++] = nums[i];
+            newArr[j++] = nums[i+n];
+        }
+        return newArr;
+    }
+}
