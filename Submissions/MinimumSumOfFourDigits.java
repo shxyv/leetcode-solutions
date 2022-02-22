fc5d0bcf5b2c9af2c446b20abe5e4906e1c9563d
shxyv <ilgar.shixiyevv@gmail.com>
2022-02-22 09:59:27 +0400

    Added MinimumSumOfFourDigits solution

---
+++ Submissions/MinimumSumOfFourDigits.java
@@ -0,0 +1,17 @@
+package Submissions;
+
+import java.util.Arrays;
+
+public class MinimumSumOfFourDigits {
+    public static void main(String[] args) {
+        System.out.println(minimumSum(2932));
+    }
+
+    public static int minimumSum(int num) {
+        char [] digitChar = (""+num).toCharArray();
+        Arrays.sort(digitChar);
+        return Integer.parseInt((""+digitChar[0]+digitChar[2])) + Integer.parseInt((""+digitChar[1]+digitChar[3]));
+    }
+
+
+}
